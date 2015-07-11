41b1027167909989cbf5736608293fb5d84cda3d
Denis Ivanets <denself@gmail.com>
2015-07-11 20:12:41 +0300

    ADd button entity

--- models/entities.py
+++ models/entities.py
@@ -1,30 +1,97 @@
 import pygame
+from managers import InputManager
 from resources import ResourceManager, Texture
 
 __author__ = 'denis'
 
 
 class BaseEntity(object):
-    def __init__(self):
+    """ Base entity, defines interface for all children """
+
+    def update(self, *args, **kwargs):
         pass
 
-    def update(self):
-        pass
-
-    def draw(self, surface):
+    def draw(self, *args, **kwargs):
         pass
 
 
-class PrinterEntity(BaseEntity):
-    def __init__(self, text):
-        super(self.__class__, self).__init__()
-        self.text = text
+class BaseButtonEntity(BaseEntity):
+    """ Base class for all buttons"""
 
-    def update(self):
+    def __init__(self, top=0, left=0, width=120, height=80):
+        super(BaseButtonEntity, self).__init__()
+        self.geometry = pygame.Rect(top, left, width, height)
+        self.inspector = InputManager()
+
+    def update(self, *args, **kwargs):
+        """ This function called from level update method.
+        """
+        if self.clicked():
+            self.callback()
+
+    def draw(self, surface, *args, **kwargs):
+        """ This function called from level draw method.
+        """
+        surface.blit(self.get_surface(), self.geometry.topleft)
+
+    def is_mouse_over(self):
+        """ Checks whether mouse over
+        :rtype: bool
+        """
+        return self.geometry.collidepoint(*self.inspector.mouse_pos)
+
+    def is_mouse_pressed(self):
+        """ Checks whether mouse pressed
+        :rtype: boot
+        """
+        return self.is_mouse_over() and self.inspector.mouse_pressed[0]
+
+    def get_color(self):
+        """ Return button color depended on button state
+        :return: Color set
+        :rtype: tuple
+        """
+        return 25, 25, 25
+
+    def get_surface(self):
+        """ Return calculated button surface
+        :rtype: pygame.Surface
+        """
+        color = self.get_color()
+        surface = pygame.Surface(self.geometry.size)
+        surface.fill(color)
+        return surface
+
+    def clicked(self):
+        """ Check whether mouse was clicked on this button.
+        :rtype: bool
+        """
+        return self.is_mouse_over() and \
+            self.inspector.mouse_button_change[0] and \
+            not self.inspector.mouse_pressed[0]
+
+    def callback(self):
+        """ callback function, called if mouse clicked
+        """
         pass
 
-    def draw(self, surface):
-        print self.text
+
+class TestButton(BaseButtonEntity):
+
+    def callback(self):
+        print 'Clicked!'
+
+    def get_color(self):
+        """ Return button color depended on button state
+        :return: Color set
+        :rtype: tuple
+        """
+        if self.is_mouse_pressed():
+            return 0xFF, 0x00, 0x00
+        elif self.is_mouse_over():
+            return 0xD1, 0xCD, 0xCD
+        else:
+            return 0xB3, 0xB6, 0xB6
 
 
 class ImageEntity(BaseEntity):
--- models/level.py
+++ models/level.py
@@ -1,3 +1,4 @@
+import os
 from pygame import Color
 from pygame.rect import Rect
 from models.entities import ImageEntity, LabelEntity
@@ -22,9 +23,9 @@ class BaseLevel(object):
         for entity in self.entities:
             entity.update()
 
-    def draw(self, screen):
+    def draw(self, surface):
         for entity in self.entities:
-            entity.draw(screen)
+            entity.draw(surface)
 
 
 class MenuLevel(BaseLevel):
@@ -33,5 +34,7 @@ class MenuLevel(BaseLevel):
 
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.entities.append(ImageEntity("Data\\test.png", Rect(0, 0, 100, 100)))
+        base_dir = os.path.dirname(os.path.dirname(__file__))
+        image_path = os.path.join(base_dir, "Data/test.png")
+        self.entities.append(ImageEntity(image_path, Rect(0, 0, 100, 100)))
         self.entities.append(LabelEntity(300, 100, "hello world!", "Times New Roman", 32, Color(255, 0, 0)))
